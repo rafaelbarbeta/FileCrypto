697209d018da2fca989188ea5d00ab7d2ea836b0
Rafael Barbeta <rafa.barbeta@gmail.com>
2021-12-29 18:50:37 -0300

    implements the encrypt and decrypt function, but with bugs

--- funcs.py
+++ funcs.py
@@ -1,15 +1,31 @@
 import base64
 
-
 class Vigenere64():
     
     ascii_values_list = [] #defines a list were the ascii values representing the base64 characters are going to be stored
+    key_decomposed = [] #like the assci_values_list, this class variable will be used to stored the key broken into pieces
+    base64_symbols = [i for i in range(48,123) if i not in range(58,65) and i not in range(91,97)] #all alfanumeric e alphabetci characters
+    base64_symbols.append(43) # append +
+    base64_symbols.append(47) # append /
+    bse64_symbols_values = [i for i in range(64)] # this list will be used to create a dictionary with the "base64_symbols" to use the vigenère cipher
 
-    def __init__(self, bytes_image): #the bytes of the file open is stored in the "bytes_image" instance variable
+    def __init__(self, bytes_image,key): #the bytes of the file open is stored in the "bytes_image" instance variable
         self.bytes_image = bytes_image
+        self.ascii_remaped = dict(zip(self.base64_symbols,self.bse64_symbols_values))
+        self.key_len = len(key)
+        for i in range(self.key_len): #split the key in characters, the get it's ascii corresponding number
+            self.key_decomposed.append(ord(key[i]))
+            self.key_decomposed[i] = self.ascii_remaped.get(self.key_decomposed[i]) #remaps the number according with the dictionary "ascci_remaped"
+        #print(self.key_decomposed)
+
+    def get_key(self, val): # Not the best approach to get the keys from values. To be changed in further commits
+        for key, value in self.ascii_remaped.items():
+             if val == value:
+                  return key
 
     def byte_to_b64(self): #converts an image to base64 encoding and returns an list with the ASCII values for the image
         self.ascii_values_list = list(base64.b64encode(self.bytes_image))
+        #print(self.ascii_values_list)
 
     def b64_to_byte(self): #converts an list with ASCII values into the machine readable bytes
         for i in range(len(self.ascii_values_list)) :
@@ -17,6 +33,33 @@ class Vigenere64():
         text_string = "".join(self.ascii_values_list)
         bytes_decoded = base64.b64decode(bytes(text_string,"ascii"))
         return bytes_decoded
+
+    def encrypt(self):
+        for i in range(len(self.ascii_values_list)):
+            #print(self.ascii_values_list)
+            mapped_value = self.ascii_remaped.get(self.ascii_values_list[i])
+            if mapped_value == None: #if the mapped value is None, that means the padding "=" has been reached, and therefore the encrypt function has fineshed it's job
+                break
+            #print(mapped_value)
+            encrypted_mapped_value = (mapped_value + self.key_decomposed[i % self.key_len]) % 64
+            #print(encrypted_mapped_value)
+            encrypted_ascii_value = self.get_key(encrypted_mapped_value)
+            #print(encrypted_ascii_value)
+            self.ascii_values_list[i] = encrypted_ascii_value
+
+    def decrypt(self):
+        for i in range(len(self.ascii_values_list)):
+            #print(self.ascii_values_list)
+            mapped_value = self.ascii_remaped.get(self.ascii_values_list[i])
+            if mapped_value == None: #if the mapped value is None, that means the padding "=" has been reached, and therefore the encrypt function has fineshed it's job
+                break
+           # print(mapped_value)
+            encrypted_mapped_value = (mapped_value - self.key_decomposed[i % self.key_len]) % 64
+            #print(encrypted_mapped_value)
+            encrypted_ascii_value = self.get_key(encrypted_mapped_value)
+            #print(encrypted_ascii_value)
+            self.ascii_values_list[i] = encrypted_ascii_value
+
 
 
 
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from funcs import Vigenere64
 print("\033[31mImgCrypto - Python3 script to encrypt images with the Vigenère Cipher\033[0m")
 
 img_to_convert = input("Please, select an image from your local folder: ") #Opena a iamge in binary mode for reading
+key = input("Please, type the key you want to use to encrypt/decrypt: ")
 with open(img_to_convert,"rb") as bin_to_convert:
     bytes_like = bin_to_convert.read() #bytes_like variable contains the binary data of the chosen image
 
-ving = Vigenere64(bytes_like)
+ving = Vigenere64(b'HelloWorld',key)
 
 print("Options available: ") # Menu
 print("1) Encrypt ")
@@ -16,12 +17,13 @@ option = input("Option (number or word): ")
 option = option.lower()
 if option == "1" or option == "encrypt":
     ving.byte_to_b64() # encode image in base64
+    ving.encrypt()
     test_variable = ving.b64_to_byte() #just for debugging reasons
 
 elif option == "2" or option == "decrypt":
     ving.byte_to_b64()
+    ving.decrypt()
     test_variable = ving.b64_to_byte()
 
-
-with open("output.png","wb") as bin_converted: # writrs the bytes from the encrypted/decrypted file in "output.png"
-    bin_converted.write(test_variable)
+with open("textoplano","wb") as bin_converted: # writes the bytes from the encrypted/decrypted file in "output.png"
+   bin_converted.write(test_variable)
