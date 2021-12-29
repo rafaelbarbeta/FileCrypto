9a8db0fe77aedf188e9c205e49eb86ea09fd22b1
Rafael Barbeta <rafa.barbeta@gmail.com>
2021-12-29 14:11:20 -0300

    organiza em classes o arquivo funcs.py

--- funcs.py
+++ funcs.py
@@ -1,15 +1,22 @@
 import base64
 
-def byte_to_b64(byte): #converts an image to base64 encoding and returns an list with the ASCII values for the image
-    text_base64 = list(base64.b64encode(byte))
-    return text_base64
 
-def b64_to_byte(text_list): #converts an list with ASCII values into the machine readable bytes
-    for i in range(len(text_list)) :
-        text_list[i] = chr(text_list[i])
-    text_string = "".join(text_list)
-    bytes_decoded = base64.b64decode(bytes(text_string,"ascii"))
-    return bytes_decoded
+class Vigenere64():
+    
+    ascii_values_list = [] #defines a list were the ascii values representing the base64 characters are going to be stored
+
+    def __init__(self, bytes_image): #the bytes of the file open is stored in the "bytes_image" instance variable
+        self.bytes_image = bytes_image
+
+    def byte_to_b64(self): #converts an image to base64 encoding and returns an list with the ASCII values for the image
+        self.ascii_values_list = list(base64.b64encode(self.bytes_image))
+
+    def b64_to_byte(self): #converts an list with ASCII values into the machine readable bytes
+        for i in range(len(self.ascii_values_list)) :
+            self.ascii_values_list[i] = chr(self.ascii_values_list[i])
+        text_string = "".join(self.ascii_values_list)
+        bytes_decoded = base64.b64decode(bytes(text_string,"ascii"))
+        return bytes_decoded
 
 
 
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import funcs
+from funcs import Vigenere64
 
 print("\033[31mImgCrypto - Python3 script to encrypt images with the Vigenère Cipher\033[0m")
 
 img_to_convert = input("Please, select an image from your local folder: ") #Opena a iamge in binary mode for reading
 with open(img_to_convert,"rb") as bin_to_convert:
     bytes_like = bin_to_convert.read() #bytes_like variable contains the binary data of the chosen image
+
+ving = Vigenere64(bytes_like)
 
 print("Options available: ") # Menu
 print("1) Encrypt ")
@@ -13,12 +15,12 @@ option = input("Option (number or word): ")
 
 option = option.lower()
 if option == "1" or option == "encrypt":
-    file_converted = funcs.byte_to_b64(bytes_like) # encode image in base64
-    test_variable = funcs.b64_to_byte(file_converted) #just for debugging reasons
+    ving.byte_to_b64() # encode image in base64
+    test_variable = ving.b64_to_byte() #just for debugging reasons
 
 elif option == "2" or option == "decrypt":
-    file_converted = funcs.byte_to_b64(bytes_like)
-    test_variable = funcs.b64_to_byte(file_converted)
+    ving.byte_to_b64()
+    test_variable = ving.b64_to_byte()
 
 
 with open("output.png","wb") as bin_converted: # writrs the bytes from the encrypted/decrypted file in "output.png"
